0d7ca804910cad3b11eb180f89f902cc88bf60ac
EmmanuelPA <emmanuel.pecha@gmail.com>
2019-12-02 14:49:35 -0600

    MergeDeckpersonalizadov4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,43 +11,21 @@ class UserController extends Controller
     //
 	     public function store(Request $request)
 	    {
-	    	if(is_null($request->name)){
-	    		     return response()->json([
+	    	 $credentials = $this->validate($request,
+        [
+            'email'=>'email|required|string',
+            'password' => 'required|string',
+            'name'=> 'required|string'
+        ]);
+	    	if($credentials){
 
-	               "errors"=> ["code"=> "422",
-	               "title"=>  "Unprocessable Entity",
-	               "description"=> "Name missing"
-	               ]]  , 422);
-	    	}elseif (is_null($request->password)) {
-	    		     return response()->json([
-
-	               "errors"=> ["code"=> "422",
-	               "title"=>  "Unprocessable Entity",
-	               "description"=> "Password missing"
-	               ]]  , 422);
-	    	}elseif (is_null($request->email)) {
-	    		     return response()->json([
-
-	               "errors"=> ["code"=> "422",
-	               "title"=>  "Unprocessable Entity",
-	               "description"=> "Email missing"
-	               ]]  , 422);
-	    	}else{
-
-
-	    	 $usuario = DB::table('users')->insert([
-	    'email' => $request->email,
-	    'name' => $request->name,
-	    'password' => $request->password,
-	    'remember_token' => Str::random(10)
-	]);
+	       	$usuario = User::create(request()->all());
 	    	 $resultado = [
 
 	               "user"=> ["name"=> $request->name,
 	               "email"=>  $request->email
 	               
 	               ]];
-	       // $usuario = User::create(request()->all());
 	        return response()->json($resultado, 201);
 	    	}
 	    }
